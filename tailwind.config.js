d81ad196edf069e425c1378dc3064ef1760ed9e8
Lucas Sierota <lucasemanuelss@hotmail.com>
2022-02-09 09:06:15 -0300

    feat: setup tailwind and btn styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-blue": "#5AADFF", 
+        "light-purple": "#CA94FF",
+      }
+    },
   },
   plugins: [],
-}
+};
