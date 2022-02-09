17f27cbf019333934c52b63382ebe853437ae759
Lucas Sierota <lucasemanuelss@hotmail.com>
2022-02-09 10:39:09 -0300

    fix: make mobile menuIcon visible

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ export const Header = () => {
             </a>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
-            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-transparent p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <Popover.Button className="inline-flex text-white items-center justify-center rounded-md bg-transparent p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
               <span className="sr-only">Open menu</span>
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
