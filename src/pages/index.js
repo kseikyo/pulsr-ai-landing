409f367fa3ab52da090f67e3cd277a72bbc76130
Lucas Sierota <lucasemanuelss@hotmail.com>
2022-02-10 09:14:24 -0300

    feat: add card for early members

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Features } from "../components/Features";
 import { Footer } from "../components/Footer";
 import { GifContainer } from "../components/GifContainer";
@@ -18,6 +19,24 @@ export default function Home() {
         <div className="my-48">
           <div className="px-8 md:px-20">
             <Features />
+          </div>
+        </div>
+      </div>
+      <div className="mt-12 mb-48 flex items-center justify-center px-8 backdrop-blur-3xl">
+        <div className="rounded-xl border-[1px] border-white/[51%] bg-white/10 p-8 shadow md:w-2/3 md:p-16 ">
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex items-center justify-center gap-4">
+              <Image src="/svgs/click.svg" width={40} height={40} alt="Join us clicking" />
+              <span className="text-lg font-medium text-white md:text-2xl">
+                Become an Early Member
+              </span>
+            </div>
+            <div className="text-center text-sm text-gray md:max-w-md md:text-lg">
+              Be the first to know of our curated drops and experience the future of NFT discovery.
+            </div>
+            <a href="#" className="btn-cta mt-6 px-16 text-lg font-medium">
+              Join Here
+            </a>
           </div>
         </div>
       </div>
